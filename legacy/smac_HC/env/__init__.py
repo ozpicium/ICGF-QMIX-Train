1b501732dbc5868909a8b3039656a68d1f3d5558
Shubham Pateria <33562819+ozpicium@users.noreply.github.com>
2022-07-09 17:07:48 +0800

    Delete legacy directory

--- legacy/smac_HC/env/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from smac_HC.env.multiagentenv import MultiAgentEnv
-from smac_HC.env.starcraft2.starcraft2_HC import StarCraft2Env_HC
-
-__all__ = ["MultiAgentEnv", "StarCraft2Env_HC"]
